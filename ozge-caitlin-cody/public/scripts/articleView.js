fbebcfd54ea1e38d0796dd3c99c4a0dd54abeea1
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-18 12:23:56 -0800

    we did changes on compile

--- ozge-caitlin-cody/public/scripts/articleView.js
+++ ozge-caitlin-cody/public/scripts/articleView.js
@@ -134,7 +134,7 @@ articleView.initAdminPage= () => {
 
   // REVIEW: We use .forEach() here because we are relying on the side-effects of the callback function: appending to the DOM. The callback is not required to return anything.
   var template  = app.Article.numWordsByAuthor().forEach(stat => $('.author-stats').append(template(stat)));
-    Handlebars.compile(template).text();//#stats
+  Handlebars.compile(template).text();//#stats
   // REVIEW: Simply write the correct values to the page:
   $('#blog-stats .articles').text(app.Article.all.length);
   $('#blog-stats .words').text(app.Article.numWordsAll());
