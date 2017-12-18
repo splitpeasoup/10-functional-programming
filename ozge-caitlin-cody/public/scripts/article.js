44040a5a1565a02e65ed2ee647e4372d56957dd7
ozge kantar boyraz <ozge.kantar@gmail.com>
2017-12-18 10:11:36 -0800

    handlebar is added

--- ozge-caitlin-cody/public/scripts/article.js
+++ ozge-caitlin-cody/public/scripts/article.js
@@ -2,7 +2,13 @@
 var app = app || {};
 
 function Article(rawDataObj) {
-  // REVIEW: In Lab 8, we explored a lot of new functionality going on here. Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object. However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode. As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object. One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context. That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function. As a result, we no longer have to pass in the optional "this" argument to forEach!
+  // REVIEW: In Lab 8, we explored a lot of new functionality going on here.
+  // Let's re-examine the concept of context. Normally, "this" inside of a constructor function refers to the newly instantiated object.
+  // However, in the function we're passing to forEach, "this" would normally refer to "undefined" in strict mode.
+  //As a result, we had to pass a second argument to forEach to make sure our "this" was still referring to our instantiated object.
+  //One of the primary purposes of lexical arrow functions, besides cleaning up syntax to use fewer lines of code, is to also preserve context.
+  //That means that when you declare a function using lexical arrows, "this" inside the function will still be the same "this" as it was outside the function.
+  //As a result, we no longer have to pass in the optional "this" argument to forEach!
   Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
 }
 
@@ -53,7 +59,9 @@ Article.truncateTable = callback => {
     method: 'DELETE',
   })
     .then(console.log)
-  // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function! The reason we can do this has to do with the way Promise.prototype.then() works. It's a little outside the scope of 301 material, but feel free to research!
+  // REVIEW: Check out this clean syntax for just passing 'assumed' data into a named function!
+  // The reason we can do this has to do with the way Promise.prototype.then() works.
+  // It's a little outside the scope of 301 material, but feel free to research!
     .then(callback);
 };
 
